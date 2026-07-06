00b4f55b05585eeccef7c54a096651d91a70f612
Ahmed Khaled <ahmedkhaled910000@gmail.com>
2026-07-06 10:30:59 +0300

    enhance landing

--- src/app/feature/landing/landing.ts
+++ src/app/feature/landing/landing.ts
@@ -19,25 +19,25 @@ export class LandingComponent {
   features = signal([
     {
       key: 'sla',
-      icon: 'M12 8v4l3 3M12 2a10 10 0 1010 10 10 10 0 00-10-10z',
+      icon: 'timer',
       titleKey: 'landing.features.sla.title',
       descKey:  'landing.features.sla.description'
     },
     {
       key: 'channels',
-      icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z',
+      icon: 'dynamic_feed',
       titleKey: 'landing.features.channels.title',
       descKey:  'landing.features.channels.description'
     },
     {
       key: 'workflow',
-      icon: 'M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15',
+      icon: 'rule_settings',
       titleKey: 'landing.features.workflow.title',
       descKey:  'landing.features.workflow.description'
     },
     {
       key: 'reports',
-      icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
+      icon: 'analytics',
       titleKey: 'landing.features.reports.title',
       descKey:  'landing.features.reports.description'
     }
